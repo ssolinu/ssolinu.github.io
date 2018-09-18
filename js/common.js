ad2fb135184cbe25a8614d495ad4facdac029c2c
linu4u <imlinu4u@gmail.com>
2018-09-18 17:30:45 +0900

    180918-173044 -

---
+++ js/common.js
@@ -0,0 +1,19 @@
+
+
+// 상단 탑 세로 길이 지침 영역
+var isTime1 = 0;
+var isTime2 = 0;
+var isTime3 = 0;
+
+$(function () {
+  $(window).scroll(function () {
+    // 스크롤 값을 취득
+    var value = $(this).scrollTop();
+    var docHeight = $(document).height();
+    var winHeight = $(window).height();
+    var percent = parseInt((value / (docHeight - winHeight)) * 100);
+    $(".percent_bar").css("left", percent + "%");
+  });
+});
+
+// 상단 탑 세로 길이 지침 영역
