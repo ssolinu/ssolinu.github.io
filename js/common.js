30c710ba4c83e354e1d7811e4f921a29daba1e67
리누 <imlinu4u@gmail.com>
2018-09-27 10:51:54 +0900

    180927-105154 -

--- js/common.js
+++ js/common.js
@@ -45,22 +45,3 @@ $(".owl-carousel").owlCarousel({
 
 });
 // 기본 자동 실행 코드 
-
-//리플 달기 부분
-/**
-*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
-/*
-var disqus_config = function () {
-this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
-this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-};
-*/
-(function () { // DON'T EDIT BELOW THIS LINE
-  var d = document, s = d.createElement('script');
-  s.src = 'https://ssolinu.disqus.com/embed.js';
-  s.setAttribute('data-timestamp', +new Date());
-  (d.head || d.body).appendChild(s);
-})();
-
-//리플 달기 부분
