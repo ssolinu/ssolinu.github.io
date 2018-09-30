af55e49a4c096d1891639f445ff029ddbd14f7a1
리누 <imlinu4u@gmail.com>
2018-09-30 17:50:14 +0900

    180930-175014 -

--- js/common.js
+++ js/common.js
@@ -1,13 +1,21 @@
 
 // D데이 타이머 
-$('#clock').countdown('2018/11/10 16:00:00', function (event) {
+$('#clock').countdown('2018/11/10 16:00:00')
+  .on('update.countdown', function (event) {
   $(this).html(
     event.strftime(''
+      + '<h3>하나가 되는날 까지</h3>'
       + '<span>%D</span> 일 '
       + '<span>%H</span> 시 '
       + '<span>%M</span> 분 '
       + '<span>%S</span> 초'));
-});
+  })
+  .on('finish.countdown', function (event) {
+    $(this).html('<h2>우리의 결혼을<br>축복해 주세요 ^^</h2>')
+  });
+
+  
+
 // D데이 타이머 
 
 // 신랑신부 토글
@@ -19,7 +27,6 @@ $(".box").click(function () {
   $(this).addClass("on");
 });
 // 신랑신부 토글
-
 
 // 기본 자동 실행 코드 
 $(document).ready(function () {
@@ -42,26 +49,30 @@ if (w < 1025) {
 //PC검출 적용
 
 
-// 갤러리 실행
-$(".owl-carousel").owlCarousel({
-  items: 1,
-  loop: true,
-  autoplay: true,
-  autoplayTimeout: 10000,
-  autoplayHoverPause: true
+
+//갤러리 인포 토글 
+var owl = $('.owl-carousel');
+owl.on('drag.owl.carousel dragged.owl.carousel ',
+  function (e) {
+    $(".gallery_info").css("display", "none");
 });
+//갤러리 인포 토글 
+
+// 갤러리 실행
+  $(".owl-carousel").owlCarousel({
+  items: 1,
+  center: true,
+  loop: true,
+  nav: false,
+  // autoplay: false,
+  // autoplayTimeout: 10000,
+  // autoplayHoverPause: true
+  });
+  
 // 갤러리 실행
 
 
 
 });
 // 기본 자동 실행 코드 
-
-window.onresize = resize;
-function resize() {
-  var w = document.body.clientWidth;
-  if (w > 767) {
-    location.href = 'index.html';
-  }
-} 
 
